949ec0de120acfd33c682bd842e042cd891065c1
YefraSoft <yefrasoft@hotmail.com>
2025-12-05 07:14:29 -0600

    ADD: Se creo un componente ButtonLoader.xaml, para poder dar feedback al usuario cuando se esta ejecutando una tarea
    - Se le agregaron bindable Propertys para modificar el command y los textos.
    - Se genero un BoolConverter para sichear que componente se mostra deacuedo a su propiedad IsBussy.

---
+++ Resources/Controls/ButtonLoader.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HuesarioApp.Resources.Controls;
+
+public partial class ButtonLoader : ContentView
+{
+    public ButtonLoader()
+    {
+        InitializeComponent();
+    }
+
+    public static readonly BindableProperty ButtonTextProperty =
+        BindableProperty.Create(
+            nameof(ButtonText),
+            typeof(string),
+            typeof(ButtonLoader),
+            defaultValue: "Button");
+
+    public string ButtonText
+    {
+        get => (string)GetValue(ButtonTextProperty);
+        set => SetValue(ButtonTextProperty, value);
+    }
+
+    public static readonly BindableProperty LoaderTextProperty =
+        BindableProperty.Create(
+            nameof(LoaderText),
+            typeof(string),
+            typeof(ButtonLoader),
+            defaultValue: "Loading...");
+
+    public string LoaderText
+    {
+        get => (string)GetValue(LoaderTextProperty);
+        set => SetValue(LoaderTextProperty, value);
+    }
+
+    public static readonly BindableProperty IsBusyProperty =
+        BindableProperty.Create(
+            nameof(IsBusy),
+            typeof(bool),
+            typeof(ButtonLoader),
+            defaultValue: false);
+
+    public bool IsBusy
+    {
+        get => (bool)GetValue(IsBusyProperty);
+        set => SetValue(IsBusyProperty, value);
+    }
+
+    public static readonly BindableProperty CommandProperty =
+        BindableProperty.Create(
+            nameof(Command),
+            typeof(ICommand),
+            typeof(ButtonLoader));
+
+    public ICommand Command
+    {
+        get => (ICommand)GetValue(CommandProperty);
+        set => SetValue(CommandProperty, value);
+    }
+}
